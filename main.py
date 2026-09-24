51c3143d23e0602acb27f04b076e7201e94e88b8
tauseefarshad1 <tauseefarshad719@gmail.comD>
2026-09-24 02:31:44 -0700

    imported read_file function

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 from text_statistics import words_counter, characters_counter, sentence_counter
 from word_analysis import get_words_frequency, get_unique_words, most_common_words
+from file_handler import read_file
 
-text = str(input("Enter text here: "))
+text = read_file("sample.txt")
 print ('<-------Text Statistics------->')
-
-print('\n\n')
-print('\n\n')
-
+print('\n')
 print('Total Words: ', words_counter(text))
-print('\n')
 print('Total Characters: ', characters_counter(text))
-print('\n')
 print('Total Sentences: ', sentence_counter(text))
-
-print('\n\n')
-print('\n\n')
-
+print('\n')
+print("<-------Word Analysis------->")
+print('\n')
 print('Words frequency: \n\n', get_words_frequency(text))
 print('\n')
 print('Unique Words: \n\n', get_unique_words(text))
